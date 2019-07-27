7cba8ad2ca47e3eaa4782de56d86e6d1d76f8d3e
ibautista007 <anime.mxa@gmail.com>
2019-07-27 13:29:11 -0700

    Creacion de Base de datos para tomar ordenes de pastel. Y creacion de primera pagina web dentro de la aplicacion

--- cOrderC/models.py
+++ cOrderC/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Designs(models.Model):  
+    nSerie = models.CharField(max_length=10)  
+    name = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')  
+    inventory = models.IntegerField()
+    setAlarm = models.IntegerField(default=5)
+  
+class Shape(models.Model):  
+    name = models.CharField(max_length=30)
+
+class Bread(models.Model):  
+    name = models.CharField(max_length=30)
+
+
